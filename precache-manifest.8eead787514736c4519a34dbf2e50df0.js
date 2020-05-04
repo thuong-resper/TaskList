ccf7d9e83e7dccd2d1fd129fd088e2a216d11e8d
Thuong <thuongresper@gmail.com>
2020-05-04 11:43:23 +0700

    Updates

---
+++ precache-manifest.8eead787514736c4519a34dbf2e50df0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1beab596d27dffe037ac83929a33dd36",
+    "url": "/TaskList/index.html"
+  },
+  {
+    "revision": "a70da80dc94e69a89aac",
+    "url": "/TaskList/static/css/main.f8318501.chunk.css"
+  },
+  {
+    "revision": "d4b05d48aad8f05dd18a",
+    "url": "/TaskList/static/js/2.0b60f248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskList/static/js/2.0b60f248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70da80dc94e69a89aac",
+    "url": "/TaskList/static/js/main.f701bfed.chunk.js"
+  },
+  {
+    "revision": "505266772a8eca8af4b3",
+    "url": "/TaskList/static/js/runtime-main.285ab799.js"
+  },
+  {
+    "revision": "0459e51e92d5ec3e388e20484d160feb",
+    "url": "/TaskList/static/media/Flaticon.0459e51e.woff"
+  },
+  {
+    "revision": "0715a16e5480ea844d3b329c9ff5004d",
+    "url": "/TaskList/static/media/Flaticon.0715a16e.ttf"
+  },
+  {
+    "revision": "2a531b1073d0771b2f1b61a913654570",
+    "url": "/TaskList/static/media/Flaticon.2a531b10.eot"
+  },
+  {
+    "revision": "452e42ac00daf6d1d808a34b4ce46854",
+    "url": "/TaskList/static/media/Flaticon.452e42ac.woff2"
+  },
+  {
+    "revision": "7b7b1171756777edaae6d87fa6785c98",
+    "url": "/TaskList/static/media/Flaticon.7b7b1171.svg"
+  }
+]);
